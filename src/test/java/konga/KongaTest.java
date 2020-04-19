31d77db38b472dcdfb5a1893a6f4b49802ac8dc0
Uche Emeka <uche.c.emeka@gmail.com>
2020-04-19 02:36:20 +0100

    Second Commit

--- src/test/java/konga/KongaTest.java
+++ src/test/java/konga/KongaTest.java
@@ -15,25 +15,26 @@ public class KongaTest extends BasePage{
         String cardNumber = "5334567822334456", date = "02/20", cvv = "254",item = "Drinks";
 
         try {//Sign in to ​Konga
-            LoginPage loginPage = homePage.clickLoginSignUpButton();
+            //Replaced the class type name 'LoginPage' with 'var'
+            var loginPage = homePage.clickLoginSignUpButton();
             //Replace the username and password string with valid user details
             loginPage.enterUsername(userId);
             loginPage.enterPassword(password);
             //Click on Login Button
             loginPage.clickLoginButton();
             //Select any of the categories and subcategory
-            SubCategoryPage subCategoryPage = homePage.clickCategoryAndSub();
+            var subCategoryPage = homePage.clickCategoryAndSub();
             //Use the search box, search for an item
             subCategoryPage.searchForAnItem(item);
             SearchPage searchPage = subCategoryPage.clickSearchButton();
             //Click on the add to cart button
             searchPage.clickOnAddToCart(addToCartButtonID);
             //Click on the My Cart Menu
-            AddToCartPage addToCartPage = searchPage.clickMyCartButton();
+            var addToCartPage = searchPage.clickMyCartButton();
             //Increase the number of item to 4
             addToCartPage.itemCount(numberOfItems);
             //Click on checkout
-            CheckOutPage checkOutPage = addToCartPage.clickCheckOut();
+            var checkOutPage = addToCartPage.clickCheckOut();
             //Select the Pay Now Option to activate the “Continue to Payment” button - the user must have preselected pick up method
             checkOutPage.clickOnPayNow();
             //Click on the “Continue to Payment” button
